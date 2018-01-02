d3fe917ef48382fd4efb1e0091e62bc6c34fa698
xieyougen <xieyougen@tuandai.com>
2018-01-02 11:29:47 +0800

    init

---
+++ src/main/java/com/dgut/TeachingplatApplication.java
@@ -0,0 +1,12 @@
+package com.dgut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachingplatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachingplatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dgut/TeachingplatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dgut;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeachingplatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
